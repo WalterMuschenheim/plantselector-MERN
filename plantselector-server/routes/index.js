8915296e328bbff4e7496ae481c3d2f42c789a8c
Walter Muschenheim <walter.muschenheim@gmail.com>
2021-12-08 14:06:27 -0500

    adds path variable to index.js route

--- plantselector-server/routes/index.js
+++ plantselector-server/routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var indexRouter = express.Router();
+var path = require("path");
 
 /* GET home page. Avoid 404s caused by BrowserRouter */
 indexRouter.get("/*", function (req, res, next) {
