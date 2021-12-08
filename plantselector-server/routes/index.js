9ba8865b171a6ffbf360d4db678058393afc2911
Walter Muschenheim <walter.muschenheim@gmail.com>
2021-12-08 13:05:25 -0500

    modifies deploy and start script to use versions of server and react app installed in node_modules

--- plantselector-server/routes/index.js
+++ plantselector-server/routes/index.js
@@ -1,9 +1,9 @@
 var express = require("express");
-var router = express.Router();
+var indexRouter = express.Router();
 
-/* GET home page. Avoid 404s caused by BrowserRouter*/
-router.get("/*", function (req, res, next) {
+/* GET home page. Avoid 404s caused by BrowserRouter */
+indexRouter.get("/*", function (req, res, next) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-module.exports = router;
+module.exports = indexRouter;
