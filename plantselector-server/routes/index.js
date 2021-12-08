26c866d5c675919b23ef0ec2d09b092923d0e3fe
Walter Muschenheim <walter.muschenheim@gmail.com>
2021-12-08 14:17:25 -0500

    adds correct relative path to index.html from index.js route file

--- plantselector-server/routes/index.js
+++ plantselector-server/routes/index.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 /* GET home page. Avoid 404s caused by BrowserRouter */
 indexRouter.get("/*", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 module.exports = indexRouter;
