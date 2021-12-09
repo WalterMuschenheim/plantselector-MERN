e469c1887248c98f677aef5c31c161cb0fdb108a
Walter Muschenheim <walter.muschenheim@gmail.com>
2021-12-08 22:00:24 -0500

    makes baseUrl dynamic based on environment

--- plantselector-react/src/shared/baseUrl.js
+++ plantselector-react/src/shared/baseUrl.js
@@ -1,3 +1,5 @@
-const baseUrl = process.env.PORT || "http://localhost:9000/";
+const production = process.env.BASE_URL;
+const development = "http://localhost:9000/";
+const baseUrl = process.env.NODE_ENV ? production : development;
 
 export default baseUrl;
