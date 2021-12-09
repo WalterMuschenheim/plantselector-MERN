9ca187a422a474a8cc5be617fc65e2eafdf5423e
Walter Muschenheim <walter.muschenheim@gmail.com>
2021-12-09 16:46:35 -0500

    gets homepage from package.json

--- plantselector-react/src/shared/baseUrl.js
+++ plantselector-react/src/shared/baseUrl.js
@@ -1,4 +1,6 @@
-const production = process.env.BASE_URL;
+const pjson = require("../../package.json");
+
+const production = pjson.homepage;
 const development = "http://localhost:9000/";
 const baseUrl = process.env.NODE_ENV ? production : development;
 
