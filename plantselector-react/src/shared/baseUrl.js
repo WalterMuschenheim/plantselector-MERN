fa87af6071a00fa1e74c2099793ab2b0e1e9ff7b
Walter Muschenheim <walter.muschenheim@gmail.com>
2021-12-10 17:48:49 -0500

    adds dockerignore files to react and server subdirectories

--- plantselector-react/src/shared/baseUrl.js
+++ plantselector-react/src/shared/baseUrl.js
@@ -2,6 +2,7 @@ const packageJSON = require("../../package.json");
 
 const production = packageJSON.homepage;
 const development = "http://localhost:9000/";
-const baseUrl = process.env.NODE_ENV ? production : development;
+const baseUrl =
+  process.env.NODE_ENV === "production" ? production : development;
 
 export default baseUrl;
